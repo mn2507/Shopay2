ade8988e0dee38f4a383052ffc0c88bddc7c1d29
mn2507 <tsm6600@hotmail.com>
2019-08-07 12:27:48 +0800

    new

---
+++ app/src/main/java/com/example/shopay/user_login.java
@@ -0,0 +1,14 @@
+package com.example.shopay;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class user_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_login);
+    }
+}

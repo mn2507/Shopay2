fc02509003f4a4b9605e9ceae4329080511ce7be
mn2507 <tsm6600@hotmail.com>
2019-08-21 18:20:11 +0800

    changes3

--- app/src/main/java/com/example/shopay/RegisterActivity.java
+++ app/src/main/java/com/example/shopay/RegisterActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -44,6 +45,7 @@ public class RegisterActivity <FirebaseFirestore> extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_register);
+
 
         btnaccount = findViewById(R.id.btnaccount);
         input_fullname = findViewById(R.id.input_fullname);
@@ -88,6 +90,81 @@ public class RegisterActivity <FirebaseFirestore> extends AppCompatActivity {
         }
     });
     }
-
+    private boolean EmailValid(String email) {
+        //check whether Email has "@"
+        return email.contains("@");
     }
+    private boolean PasswordValid(String password) {
+        //check whether Password has minimum characters
+        return password.length() > 8;
+    }
+    private void Registeruser() {
+
+        input_email.setError(null);
+        input_password.setError(null);
+        input_fullname.setError(null);
+        input_number.setError(null);
+        input_address.setError(null);
+        input_state.setError(null);
+        input_city.setError(null);
+        input_zip.setError(null);
+        final String username = input_email.getText().toString().trim();
+        final String password = input_email.getText().toString().trim();
+        String fullname = input_fullname.getText().toString().trim();
+        String number = input_number.getText().toString().trim();
+        String address = input_address.getText().toString().trim();
+        String state = input_state.getText().toString().trim();
+        String city = input_city.getText().toString().trim();
+        String zip = input_zip.getText().toString().trim();
+
+        if (TextUtils.isEmpty(username)) {
+            Toast.makeText(this, "Please enter your username/email", Toast.LENGTH_SHORT).show();
+            return;
+        }  else if (!PasswordValid(password)) {
+            input_password.setError("Invalid username/email");
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please enter your password", Toast.LENGTH_SHORT).show();
+            return;
+        } else if (!PasswordValid(password)) {
+            input_password.setError("Weak password");
+            return;
+        }
+
+        if (TextUtils.isEmpty(fullname)) {
+            Toast.makeText(this, "Please enter your full name", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(number)) {
+            Toast.makeText(this, "Please enter your contact number", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(address)) {
+            Toast.makeText(this, "Please enter your address", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(state)) {
+            Toast.makeText(this, "Please enter your state", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(city)) {
+            Toast.makeText(this, "Please enter your city", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(zip)) {
+            Toast.makeText(this, "Please enter your zip code", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else {Toast.makeText(this, "Logged in successfully!", Toast.LENGTH_SHORT).show();
+    }
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+    }
+}
 
